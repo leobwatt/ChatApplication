f3383350fd2c703caed183ef9e4258f1ef299c45
Theawesometurtl <Shablammmo@gmail.com>
2022-01-13 13:55:39 -0800

     Conflicts: 	.idea/misc.xml

--- app/src/main/java/com/example/chatapplication/Login.kt
+++ app/src/main/java/com/example/chatapplication/Login.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
 
@@ -13,9 +15,13 @@ class Login : AppCompatActivity() {
     private lateinit var btn_login: Button
     private lateinit var btn_sign_up: Button
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edt_email = findViewById(R.id.edt_email)
         edt_password = findViewById(R.id.edt_password)
@@ -26,5 +32,30 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+        btn_login.setOnClickListener {
+            val email = edt_email.text.toString()
+            val password = edt_password.text.toString()
+
+            login(email,password)
+        }
     }
+
+    private fun login(email: String, password: String) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+
+                    Toast.makeText(this@Login, "User not found", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+    }
+
 }
